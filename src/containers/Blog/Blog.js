300d5b357b4c3050ffb73dc6760831e8d6db0597
Ali <AliSaadati619@yahoo.com>
2019-12-18 01:06:35 -0800

    First commit

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+// import axios from 'axios'
+import axios from '../../axios';
+
+import Posts from "../Blog/Posts/Posts";
+import './Blog.module.css';
+
+class Blog extends Component {
+
+
+
+    render() {
+        return (
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <Posts />
+            </div>
+        );
+    }
+}
+
+export default Blog;
