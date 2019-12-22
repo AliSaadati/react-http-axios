70265e8a503c93eaffcbc704737c3becdf13461f
Ali Saadati <AliSaadati619@yahoo.com>
2019-12-22 01:33:18 -0800

    Update

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 // import axios from 'axios'
-import axios from '../../axios';
+import { Route, Link } from 'react-router-dom';
 
 import Posts from "../Blog/Posts/Posts";
+import NewPost from "../Blog/NewPost/NewPost";
 import './Blog.module.css';
 
 class Blog extends Component {
@@ -15,12 +16,23 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">Home</a></li>
-                            <li><a href="/">New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: "/new-post",
+                                hash: '#submit',
+                                search: '?quick-submit=true'}}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
-                <Posts />
+                {/*<Route
+                    path="/"
+                    render={() => <h1>Home</h1>}
+                    exact />
+                <Route
+                    path="/"
+                render={() => <h1>Home</h1>}/>*/}
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" exact component={NewPost} />
             </div>
         );
     }
